f7cb0f10c8fc03eac86dce374c3aaed059bfb5a6
Figlesias <fernando.iglesias@edu.ozonatech.com>
2024-05-23 07:45:50 +0200

    added player repo

---
+++ PokerXestWPF/Repositories/PlayerRepository.cs
@@ -0,0 +1,42 @@
+﻿using PokerXestWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerXestWPF.Repositories
+{
+    public class PlayerRepository : RepositoryBase, IPlayerRepository
+    {
+        public void Add(PlayerModel playerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(PlayerModel playerModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PlayerModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerModel GetByDni(string dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerModel GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string dni)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
